bc3e68f22c716c0f8085dc2561043f6162653ad0
cojoigo <cojoigo@gmail.com>
2016-01-14 16:12:26 -0500

    added /user

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ var express = require('express');
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/hello', function(req, res){
+
+app.get('/hello', sayHello);
+
+app.get('/user', getAllUsers);
+
+function sayHello(req, res){
     res.send('hello world');
-});
+}
+
+function getAllUsers(req, res) {
+    var users = [
+        {username: "Bruinn", firstname: "Greg", lastName: "Bernard"},
+        {username: "TehMarchHare", firstname: "Dan", lastName: "Cardin"},
+        {username: "cojoigo", firstname: "Connor", lastName: "Igo"}
+    ];
+    res.json(users);
+}
+
 app.listen(port, ipaddress);
