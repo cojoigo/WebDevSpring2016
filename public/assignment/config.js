8d4ba69105b4881d7c78d133e50ed9cad234281a
cojoigo <cojoigo@gmail.com>
2016-02-23 13:19:21 -0500

    Saving work - views implemented and all pages being viewed from the index file

---
+++ public/assignment/config.js
@@ -0,0 +1,38 @@
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .config(function($routeProvider){
+            $routeProvider
+                .when("/", {
+                    templateUrl: "views/home/home.view.html",
+                    controller: "HomeController"
+                })
+                .when("/profile", {
+                    templateUrl: "views/users/profile.view.html",
+                    controller: "ProfileController"
+                })
+                .when("/login", {
+                    templateUrl: "views/users/login.view.html",
+                    controller: "LoginController"
+                })
+                .when("/register", {
+                    templateUrl: "views/users/register.view.html",
+                    controller: "RegisterController"
+                })
+                .when("/forms", {
+                    templateUrl: "views/forms/forms.view.html",
+                    controller: "FormsController"
+                })
+                .when("/form-fields", {
+                    templateUrl: "views/forms/form-fields.view.html",
+                    controller: "FormFieldsController"
+                })
+                .when("/admin", {
+                    templateUrl: "views/admin/admin.view.html",
+                    controller: "AdminController"
+                })
+                .otherwise({
+                    redirectTo: "/"
+                });
+        });
+})();
