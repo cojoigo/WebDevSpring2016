763a134edcd30cb20643a521ef119b4e61f185a6
cojoigo <cojoigo@gmail.com>
2016-03-26 23:09:46 -0400

    Typo in server code

--- public/project/server/app.js
+++ public/project/server/app.js
@@ -1,8 +1,8 @@
 module.exports = function(app, brewdb) {
 
     var userModel   = require("./models/user.model.js")();
-    var brewDBModel   = require("./models/brewdb.model.js")(brewdb);
+    var brewDBModel   = require("./models/brewDB.model.js")(brewdb);
 
     var userService  = require("./services/user.service.server.js") (app, userModel);
-    var brewDBService = require("./services/brewdb.service.server.js")(app, brewDBModel, userModel);
+    var brewDBService = require("./services/brewDB.service.server.js")(app, brewDBModel, userModel);
 };
